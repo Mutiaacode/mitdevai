07dd6196d419ec48d6dcfb5351c8dc32dd90da0c
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-09-30 09:47:21 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-out',
+        fadeOut: 'fadeOut 1s ease-out',
+      },
     },
   },
   plugins: [],
