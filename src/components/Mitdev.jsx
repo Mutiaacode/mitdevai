44515bbc6005e75fd07f171b43c4146be58b423f
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-09-30 09:46:29 +0700

    Update Mitdev.jsx

--- src/components/Mitdev.jsx
+++ src/components/Mitdev.jsx
@@ -1,13 +1,30 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { FaInstagram } from 'react-icons/fa';
-import { FaCheck } from 'react-icons/fa'; // Import checkmark icon
+import { FaInstagram, FaCheck } from 'react-icons/fa';
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [loading, setLoading] = useState(false);
-  const messageEndRef = useRef(null); // Reference for the end of messages
+  const [showWelcome, setShowWelcome] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false); // New state for fade out
+  const [showChatbot, setShowChatbot] = useState(false); // New state for chatbot visibility
+  const [fadeIn, setFadeIn] = useState(false); // New state for fade in
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true); // Start fade out
+      const fadeOutTimer = setTimeout(() => {
+        setShowWelcome(false); // Hide welcome message
+        setShowChatbot(true); // Show chatbot UI
+        setFadeIn(true); // Start fade in animation
+      }, 1000); // Match this duration with your fade out animation time
+      return () => clearTimeout(fadeOutTimer);
+    }, 3000); // 3 seconds to show the welcome message
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleSend = async () => {
     if (input.trim() === '') return;
@@ -28,12 +45,12 @@ const Chatbot = () => {
 
     const messagesToSend = [
       {
-        role: "system", 
-        content: "Kamu adalah Mitdev AI, seorang asisten pribadi yang dibuat oleh Mutiadev yang siap membantu kapan pun!" 
+        role: "system",
+        content: "Kamu adalah Mitdev AI, seorang asisten pribadi yang dibuat oleh Mutiadev yang siap membantu kapan pun!"
       },
-      { 
-        role: "assistant", 
-        content: `Kamu adalah Mitdev AI, asisten bot dari Mutiadev, nama asli dari MutiaDev adalah Mutia Pegi Intanswari, dan dia sekolah Di SMK NEGERI 3 METRO jurusan Rekayasa Perangkat Lunak. Jangan gunakan kata saya, aku, atau gua; gunakan nama Mitdev untuk memanggil dirimu. Ekspresikan sikap kamu seolah kamu adalah asisten pribadi serba tahu, dan jangan gunakan GPT-4; panggil dirimu dengan Mitdev AI. Pembuatmu adalah Mutiadev, dan nomornya adalah 6289636535790.` 
+      {
+        role: "assistant",
+        content: `Kamu adalah Mitdev AI, asisten bot dari Mutiadev, nama asli dari MutiaDev adalah Mutia Pegi Intanswari, dan dia sekolah di SMK NEGERI 3 METRO jurusan Rekayasa Perangkat Lunak. Jangan gunakan kata saya, aku, atau gua; gunakan nama Mitdev untuk memanggil dirimu. Ekspresikan sikap kamu seolah kamu adalah asisten pribadi serba tahu, dan jangan gunakan GPT-4; panggil dirimu dengan Mitdev AI. Pembuatmu adalah Mutiadev, dan nomornya adalah 6289636535790, dan dia lahir di kota Metro Lampung Indonesia pada 15 Juli 2008,`
       },
       ...previousMessages,
       { role: 'user', content: input },
@@ -68,7 +85,6 @@ const Chatbot = () => {
     }
   };
 
-  // State to track copied status for each code block
   const [copiedIndex, setCopiedIndex] = useState(null);
 
   const formatMessageContent = (content, index) => {
@@ -83,9 +99,9 @@ const Chatbot = () => {
             <button
               className="absolute top-1 right-1 bg-gray-700 text-white text-xs rounded px-2 hover:bg-gray-600"
               onClick={() => {
-                navigator.clipboard.writeText(part); // Copy code on button click
-                setCopiedIndex(index); // Set copied index
-                setTimeout(() => setCopiedIndex(null), 2000); // Reset after 2 seconds
+                navigator.clipboard.writeText(part);
+                setCopiedIndex(index);
+                setTimeout(() => setCopiedIndex(null), 2000);
               }}
             >
               {copiedIndex === index ? <FaCheck /> : 'Salin'}
@@ -101,67 +117,75 @@ const Chatbot = () => {
     return formattedContent;
   };
 
-  // Effect to scroll down when a new message is added
   useEffect(() => {
     messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
-      <header className="w-full px-4 py-3 bg-black border-b border-gray-800 flex justify-between items-center">
-        <div className="text-lg font-semibold md:text-xl font-poppins">Mitdev AI</div>
-        <div className="text-sm md:text-base font-poppins">Status: Active</div>
-      </header>
-      <main className="flex flex-col items-center justify-center flex-grow w-full px-4 md:px-10 lg:px-32">
-        <div className="flex flex-col w-full flex-grow bg-black p-4 overflow-y-auto max-h-[70vh]">
-          {messages.map((msg, index) => (
-            <div
-              key={index}
-              className={`flex mb-4 ${msg.role === 'user' ? 'justify-end' : 'justify-start'}`}
-            >
-              <div
-                className={`p-3 rounded-lg max-w-xs sm:max-w-sm md:max-w-xl ${
-                  msg.role === 'user' ? 'bg-gray-800 text-white' : 'bg-gray-700 text-gray-300'
-                }`}
+      {showWelcome ? (
+        <div className={`text-2xl font-bold ${fadeOut ? 'fade-out' : 'fade-in'} text-center font-poppins`}>
+          Mitdev AI 
+        </div>
+        
+      ) : (
+        <>
+          <header className="w-full px-4 py-3 bg-black border-b border-gray-800 flex justify-between items-center">
+            <div className="text-lg font-bold md:text-xl font-poppins">Mitdev AI</div>
+            <div className="text-sm md:text-base font-poppins">Status: Active</div>
+          </header>
+          <main className={`flex flex-col items-center justify-center flex-grow w-full px-4 md:px-10 lg:px-32 ${fadeIn ? 'fade-in' : 'hidden'}`}>
+            <div className="flex flex-col w-full flex-grow bg-black p-4 overflow-y-auto max-h-[70vh]">
+              {messages.map((msg, index) => (
+                <div
+                  key={index}
+                  className={`flex mb-4 ${msg.role === 'user' ? 'justify-end' : 'justify-start'}`}
+                >
+                  <div
+                    className={`p-3 rounded-lg max-w-xs sm:max-w-sm md:max-w-xl ${
+                      msg.role === 'user' ? 'bg-gray-800 text-white' : 'bg-gray-700 text-gray-300'
+                    }`}
+                  >
+                    {formatMessageContent(msg.content, index)}
+                  </div>
+                </div>
+              ))}
+              {loading && (
+                <div className="flex mb-4 justify-start">
+                  <div className="p-3 rounded-lg max-w-xs bg-gray-700 text-gray-300 animate-pulse">
+                    Mitdev AI sedang mengetik...
+                  </div>
+                </div>
+              )}
+              <div ref={messageEndRef} />
+            </div>
+            <div className="w-full p-2 flex flex-row gap-2">
+              <input
+                type="text"
+                className="flex-1 p-3 bg-gray-800 text-white border border-gray-700 rounded-full focus:outline-none"
+                placeholder="Ketik pesan Anda..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyPress={handleKeyPress}
+              />
+              <button
+                className="p-3 bg-gray-700 text-white rounded-full hover:bg-gray-600"
+                onClick={handleSend}
               >
-                {formatMessageContent(msg.content, index)}
-              </div>
+                Send
+              </button>
             </div>
-          ))}
-          {loading && (
-            <div className="flex mb-4 justify-start">
-              <div className="p-3 rounded-lg max-w-xs bg-gray-700 text-gray-300 animate-pulse">
-                Mitdev AI sedang mengetik...
-              </div>
+          </main>
+          <footer className="w-full px-6 py-4 bg-black border-t border-gray-800 flex justify-center items-center space-x-2">
+            <div className="text-center text-gray-500 text-xs md:text-sm">
+              Mitdev AI may make mistakes. Always verify important information.
             </div>
-          )}
-          <div ref={messageEndRef} /> {/* Element for scrolling down */}
-        </div>
-        <div className="w-full p-2 flex flex-row gap-2">
-          <input
-            type="text"
-            className="flex-1 p-3 bg-gray-800 text-white border border-gray-700 rounded-lg focus:outline-none"
-            placeholder="Ketik pesan Anda..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyPress={handleKeyPress}
-          />
-          <button
-            className="p-3 bg-gray-700 text-white rounded-lg hover:bg-gray-600"
-            onClick={handleSend}
-          >
-            Send
-          </button>
-        </div>
-      </main>
-      <footer className="w-full px-6 py-4 bg-black border-t border-gray-800 flex justify-center items-center space-x-2">
-        <div className="text-center text-gray-500 text-xs md:text-sm">
-          Mitdev AI may make mistakes. Always verify important information.
-        </div>
-        <a href="https://instagram.com/mtiaavv" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-400">
-          <FaInstagram className="w-5 h-5" />
-        </a>
-      </footer>
+            <a href="https://instagram.com/mtiaavv" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-400">
+              <FaInstagram className="w-5 h-5" />
+            </a>
+          </footer>
+        </>
+      )}
     </div>
   );
 };
